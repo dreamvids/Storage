3e5f9c33a9623a9431cd5d10d3f98c57114e1671
PHPeter <phpeter@phpeter.fr>
2014-06-19 12:18:40 +0200

    Previous commit doesn't work well, try this one ;p

--- config.php
+++ config.php
@@ -5,4 +5,5 @@
 
 define('PRIVATE_KEY', 'generate_your_own_key');
 define('SERVER_ID', 1);
-define('MASTER_SERV', 'your-server-addr/whit/path/');
+define('MASTER_SERV', 'your-server-addr/whit/path/without/final/slash');
+define('WORKING_DIR', '/absolute/path/to/this/directory');
--- cron.php
+++ cron.php
@@ -5,7 +5,7 @@
  * All expired accesses will be deleted by this script.
  */
 
-define ('DIR', getcwd().'incomings/'); // Define the accesses directory
+define ('DIR', WORKING_DIR.'incomings/'); // Define the accesses directory
 $MyDirectory = opendir(DIR); // Open it
 $i = 0; $j = 0; // Init counters
 
