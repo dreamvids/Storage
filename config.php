ef4b060f1f31aebb3882777dfc0ffdfe3f3c5b8b
PHPeter <phpeter@phpeter.fr>
2014-06-19 12:43:22 +0200

    little bugfix, hope this is gonna work, this time...

--- config.php
+++ config.php
@@ -6,4 +6,4 @@
 define('PRIVATE_KEY', 'generate_your_own_key');
 define('SERVER_ID', 1);
 define('MASTER_SERV', 'your-server-addr/whit/path/without/final/slash');
-define('WORKING_DIR', '/absolute/path/to/this/directory');
+define('WORKING_DIR', '/absolute/path/to/this/directory/without/final/slash');
