edd4f95603795f1f01a5f0c19b5ed637302c3f01
Vetiore <phpeter@phpeter.fr>
2014-08-25 02:50:00 +0200

    Smarter UX

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 define('ROOT', '/'); // Your disk/partition root path where you want to store data
 define('ALERT_EMAIL_ADDRESS', 'you@domain.tld'); // Set your mail here, to receive alerts
 define('UNIT', 1000000000); // GB (1 > B; 1000 > KB; 1000000 > MB; 1000000000 > GB; etc.)
+define('UNIT_NAME', 'Go') // Must be linked to the previous constant, here I use the GB unit (in France, we say Go instead of GB)
 define('CRITICAL_FREE_SPACE', 10 * UNIT); // Set your critical limit here
 
 // Get free disk space
@@ -19,10 +20,10 @@ $free_space = disk_free_space(ROOT);
 if ($free_space < CRITICAL_FREE_SPACE)
 {
 	// French
-	mail(ALERT_EMAIL_ADDRESS, '[DV Storage] Seuil critique atteint sur '.$_SERVER['SERVER_ADDR'].', Libre: '.($free_space / 1000000000).' Go');
+	mail(ALERT_EMAIL_ADDRESS, '[DV Storage] Seuil critique atteint sur '.$_SERVER['SERVER_ADDR'].', Libre: '.($free_space / UNIT).' '.UNIT_NAME);
 	
 	// English
-	//mail(ALERT_EMAIL_ADDRESS, '[DV Storage] Critical limit reached on '.$_SERVER['SERVER_ADDR'].', Free: '.($free_space / 1000000000).' GB');
+	//mail(ALERT_EMAIL_ADDRESS, '[DV Storage] Critical limit reached on '.$_SERVER['SERVER_ADDR'].', Free: '.($free_space / UNIT).' '.UNIT_NAME);
 	
 	echo 'CRITICAL_ALERT';
 }
