cc3c8194834b4c57eba73a119f046c89bee738f3
PHPeter <phpeter@phpeter.fr>
2014-10-08 22:46:39 +0200

    adapting to new V2 upload system

--- config.php
+++ config.php
@@ -3,6 +3,6 @@
  * Some configuration variables
  */
 
-define('PRIVATE_KEY', 'generate_your_own_key');
+define('PRIVATE_KEY', 'your_own_key');
 define('SERVER_ID', 1);
 define('MASTER_SERV', 'localhost/DreamVids');
--- mkdir.php
+++ mkdir.php
@@ -6,12 +6,12 @@
  */
 
 // We need our configuration variables here
-require_once '../config.php';
+require_once 'config.php';
 
 $https = ($_SERVER['SERVER_PORT'] == '443') ? 's' : ''; // Begin the building of the server url
 $url = 'http'.$https.'://'.$_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF']; // same as previous comment
 $url = preg_replace("#mkdir.php$#", "", $url); // Delete the relative path to keep only the server name
-$hash = hash_hmac('sha256', $content, PRIVATE_KEY); // Create the server Signature
+$hash = hash_hmac('sha256', $url, PRIVATE_KEY); // Create the server Signature
 
 // If the received hash is the same as the builded one
 if (@$_GET['hash'] == $hash) {
@@ -22,10 +22,8 @@ if (@$_GET['hash'] == $hash) {
 	if (preg_match("#^[a-zA-Z0-9_]+$#", $_GET['cid'])) {
 		if (!file_exists('uploads/'.$_GET['cid'].'/')) {
 			mkdir('uploads/'.$_GET['cid']);
-			mkdir('uploads/'.$_GET['cid'].'/videos');
 			echo 0;exit();
 		}
 	}
-	
-	echo 1;
 }
+echo 1;
